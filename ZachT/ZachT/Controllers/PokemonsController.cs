ed2c33d1fbe41e80a828ba096efb1b70f3ec51f4
snell <lincecum5555555555@gmail.com>
2024-06-14 19:59:21 +0800

    ef practice

---
+++ ZachT/ZachT/Controllers/PokemonsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ZachT.Models;
+
+namespace ZachT.Controllers
+{
+    public class PokemonsController : Controller
+    {
+        private JLearningEntities2 db = new JLearningEntities2();
+        // GET: Pokemons
+        public ActionResult Index()
+        {
+            return View(db.Pokemon.ToList());
+        }
+
+        // GET: Pokemons/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pokemon pokemon = db.Pokemon.Find(id);
+            if (pokemon == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pokemon);
+        }
+
+        // GET: Pokemons/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pokemons/Create
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PokeID,PokeName")] Pokemon pokemon)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pokemon.Add(pokemon);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pokemon);
+        }
+
+        // GET: Pokemons/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pokemon pokemon = db.Pokemon.Find(id);
+            if (pokemon == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pokemon);
+        }
+
+        // POST: Pokemons/Edit/5
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PokeID,PokeName")] Pokemon pokemon)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pokemon).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pokemon);
+        }
+
+        // GET: Pokemons/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pokemon pokemon = db.Pokemon.Find(id);
+            if (pokemon == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pokemon);
+        }
+
+        // POST: Pokemons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pokemon pokemon = db.Pokemon.Find(id);
+            db.Pokemon.Remove(pokemon);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
